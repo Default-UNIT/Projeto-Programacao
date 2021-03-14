6c9c0ee28eb6cb7a49899afa78906e56e54b0518
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-03-14 17:40:57 -0300

    Add files via upload

---
+++ CriarTela/model/Funcionario.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Funcionario {
+    private String codFuncionario;
+    private String nomeFuncionario;
+    private String cpfFuncionario;
+
+    public String getCodFuncionario() {
+        return codFuncionario;
+    }
+
+    public void setCodFuncionario(String codFuncionario) {
+        this.codFuncionario = codFuncionario;
+    }
+
+    public String getNomeFuncionario() {
+        return nomeFuncionario;
+    }
+
+    public void setNomeFuncionario(String nomeFuncionario) {
+        this.nomeFuncionario = nomeFuncionario;
+    }
+
+    public String getCpfFuncionario() {
+        return cpfFuncionario;
+    }
+
+    public void setCpfFuncionario(String cpfFuncionario) {
+        this.cpfFuncionario = cpfFuncionario;
+    }
+    
+}
