5f3186a13fddf43e39b17f98af044f0bc9edc100
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-03-16 17:49:06 -0300

    Create Produto.java

---
+++ CriarTela/model/Produto.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Produto {
+    private String codProduto;
+    private String nomeProduto;
+    private String catProduto;
+    private String fmlProduto;
+
+    public String getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(String codProduto) {
+        this.codProduto = codProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getCatProduto() {
+        return catProduto;
+    }
+
+    public void setCatProduto(String catProduto) {
+        this.catProduto = catProduto;
+    }
+
+    public String getFmlProduto() {
+        return fmlProduto;
+    }
+
+    public void setFmlProduto(String fmlProduto) {
+        this.fmlProduto = fmlProduto;
+    }
+}
