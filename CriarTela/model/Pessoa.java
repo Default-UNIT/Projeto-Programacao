c12f6c336c70b9a1ad5a973b9e92981ebee2a907
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-03-20 22:36:27 -0300

    Update Pessoa.java

--- CriarTela/model/Pessoa.java
+++ CriarTela/model/Pessoa.java
@@ -1,8 +1,21 @@
 package model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Pessoa {
     private String nome;
     private String cpf;
+    private String telefone;
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
     public String getNome() {
         return nome;
@@ -20,4 +33,19 @@ public class Pessoa {
         this.cpf = cpf;
     }
     
+    public void registrarCliente() {
+        try {
+            FileWriter escreverArq = new FileWriter("./cliente.txt", true);
+            PrintWriter pr = new PrintWriter(escreverArq);
+            
+            pr.println("Telefone: " + this.telefone + ";Nome: " + this.nome + ";Cpf: " + this.cpf + "\n");
+            
+            pr.flush();
+            escreverArq.close();
+            
+        } catch(IOException e) {
+            System.out.println("Erro ao cadastrar usuário!");
+        }
+    }
+
 }
