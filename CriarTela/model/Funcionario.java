3582d9a5654e408136b79bb398fa4657f549b451
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-03-20 22:36:05 -0300

    Update Funcionario.java

--- CriarTela/model/Funcionario.java
+++ CriarTela/model/Funcionario.java
@@ -1,9 +1,22 @@
 package model;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Funcionario {
     private String codFuncionario;
     private String nomeFuncionario;
     private String cpfFuncionario;
+    private String opcFuncionario;
+
+    public String getOpcFuncionario() {
+        return opcFuncionario;
+    }
+
+    public void setOpcFuncionario(String opcFuncionario) {
+        this.opcFuncionario = opcFuncionario;
+    }
 
     public String getCodFuncionario() {
         return codFuncionario;
@@ -27,6 +40,22 @@ public class Funcionario {
 
     public void setCpfFuncionario(String cpfFuncionario) {
         this.cpfFuncionario = cpfFuncionario;
+    }
+    
+    public void registrarFuncionario() {
+        try {
+            FileWriter escreverArq = new FileWriter("./funcionario.txt", true);
+            PrintWriter pr = new PrintWriter(escreverArq);
+
+            pr.println("Cargo: " + this.opcFuncionario + ";Código: " + this.codFuncionario
+            + ";Nome: " + this.nomeFuncionario + ";Cpf: " + this.cpfFuncionario);
+
+            pr.flush();
+            escreverArq.close();
+
+        } catch(IOException e) {
+            System.out.println("Erro ao cadastrar usuário!");
+        }
     }
     
 }
